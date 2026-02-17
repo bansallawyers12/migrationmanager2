0cae6fd8f0b1e22e1936ac326c1d065ee486f75b
viplucmca@yahoo.co.in <viplucmca@yahoo.co.in>
2026-02-17 14:40:18 +0530

    Refactor email modal template loading logic in client and company detail views
    - Updated JavaScript to conditionally skip template loading based on the presence of a 'fromSignatureSend' flag, improving the email composition process.
    - Enhanced the handling of template selection to ensure proper functionality when switching between templates, maintaining a seamless user experience.

--- resources/views/crm/companies/detail.blade.php
+++ resources/views/crm/companies/detail.blade.php
@@ -1329,10 +1329,14 @@ $(document).ready(function() {
                     });
                     var fromSignature = $('#emailmodal').data('fromSignatureSend');
                     var toSelect = res.template ? res.template.id : (res.matter_templates[0] ? res.matter_templates[0].id : null);
-                    if (toSelect && !fromSignature) {
-                        $templateSelect.val(toSelect).trigger('change');
-                    } else if (fromSignature) {
-                        $('#emailmodal').removeData('fromSignatureSend');
+                    if (toSelect) {
+                        if (!fromSignature) {
+                            $templateSelect.val(toSelect).trigger('change');
+                        } else {
+                            $('#emailmodal').data('skipTemplateLoad', true);
+                            $templateSelect.val(toSelect).trigger('change');
+                            $('#emailmodal').removeData('fromSignatureSend');
+                        }
                     }
                 }
                 // Show only matter-related checklists; hide all others
