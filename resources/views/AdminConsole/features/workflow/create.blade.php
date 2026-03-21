cfcba2a8510535945f68cd66cfa183c8111cba8f
bansallawyers12@gmail.com <bansallawyers12@gmail.com>
2026-03-21 21:47:32 +1100

    style(workflow): improve card header layout in stage creation view
    - Reformatted the card header in the create workflow stage view for better readability and structure.
    - Enhanced the display of the workflow name and insertion position information, improving user experience.

--- resources/views/AdminConsole/features/workflow/create.blade.php
+++ resources/views/AdminConsole/features/workflow/create.blade.php
@@ -12,8 +12,14 @@
 				<div class="row">
 					<div class="col-12 col-md-12 col-lg-12">
 						<div class="card">
-							<div class="card-header">
-							<h4>Add Workflow Stage@if(isset($workflow)) to {{ $workflow->name }}@endif @if(!empty($insertAfterStage))<small class="text-muted font-weight-normal">(after &ldquo;{{ \Illuminate\Support\Str::limit($insertAfterStage->name, 60) }}&rdquo;)</small>@endif</h4>
+						<div class="card-header">
+						<h4>
+							Add Workflow Stage
+							@if(isset($workflow)) to {{ $workflow->name }}@endif
+							@if(!empty($insertAfterStage))
+							<small class="text-muted font-weight-normal">(after &ldquo;{{ \Illuminate\Support\Str::limit($insertAfterStage->name, 60) }}&rdquo;)</small>
+							@endif
+						</h4>
 							<div class="card-header-action">
 									<a href="{{ isset($workflow) ? route('adminconsole.features.workflow.stages', base64_encode(convert_uuencode($workflow->id))) : route('adminconsole.features.workflow.index') }}" class="btn btn-primary"><i class="fa fa-arrow-left"></i> Back</a>
 							</div>
